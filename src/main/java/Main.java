80459658a8d949d43edfc5918a1afe0780024258
AhmetBeskazalioglu <ahmetbeskazalioglu@icloud.com>
2024-12-22 23:37:42 +0300

    type

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
 
-
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
@@ -13,10 +12,19 @@ public class Main {
             if (input.equals("exit 0"))
                 break;
 
-            if(input.startsWith("echo"))
+            a:
+            if (input.startsWith("echo"))
                 System.out.println(input.substring(5));
-            else
+            else if (input.startsWith("type")) {
+                if (input.startsWith("type echo") || input.startsWith("type type") || input.startsWith("type exit"))
+                    System.out.println(input.substring(5) + " is a shell builtin");
+                else
+                    System.out.println(input.substring(5) + ": not found");
+                break a;
+            } else
                 System.out.println(input + ": command not found");
+
+
         }
     }
 }
